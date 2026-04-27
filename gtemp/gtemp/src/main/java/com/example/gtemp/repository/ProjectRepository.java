a71685afba4ab2e7c76b1ed7e6a9d69507394b40
Isaac-Raphael-git <sac.x92.x68.rap2x@gmail.com>
2026-04-27 17:39:51 +0800

    Delete gtemp/gtemp directory

--- gtemp/gtemp/src/main/java/com/example/gtemp/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gtemp.repository;
-
-import com.example.gtemp.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ProjectRepository extends JpaRepository<Project, UUID> {
-}
